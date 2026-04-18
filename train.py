f59f3f9a5145e84e3442a9d3bebd9147bc68037d
Mohamed Essam <imessam1998@gmail.com>
2026-04-18 22:37:07 +0200

    merge

--- train.py
+++ train.py
@@ -13,10 +13,17 @@ class BPE:
         self._tok_EOS = "<|endoftext|>"
         self._specials = {256 : self._tok_EOS}
 
-    def pretokenize(self, corpus : List[str]) -> List:
-        return [sentence.split(" ") for sentence in corpus]
+    def _pretokenize(self, corpus : List[str]) -> List[str]:
 
-    def count(self, pre_tokens : List[str]) -> Dict[str, int]:
+        pre_tokens = []
+
+        for sentence in corpus:
+            pre_tokens.extend(sentence.split(" "))
+
+        return pre_tokens
+
+    def _count(self, pre_tokens : List[str]) -> Dict[str, int]:
+        
         words_count = {}
 
         for token in pre_tokens:
@@ -26,18 +33,63 @@ class BPE:
 
         return words_count
     
-    def count_to_bytes(self, words_count : Dict[str, int]) -> Dict[Tuple[bytes, ...], int]:
+    def _counts_to_bytes(self, words_count : Dict[str, int]) -> Dict[Tuple[bytes, ...], int]:
 
         bytes_count : dict[tuple[bytes, ...], int] = {}
+
+        for word, count in words_count.items():
+            bytes_count[tuple([ch.encode() for ch in word])] = count
 
         return bytes_count
 
 
-    def merge(self):
-        pass
+    def _merge(self, bytes_count : Dict[Tuple[bytes, ...], int]) -> Dict[Tuple[bytes, ...], int]:
+        
+        bytes_count_merged : Dict[Tuple[bytes, ...], int] = {}
 
-    def train(self):
-        pass
+        pairs = {}
+
+        for token_bytes, count in bytes_count.items():
+
+            for idx, b in enumerate(token_bytes):
+
+                if idx > 0 and idx < len(token_bytes):
+
+                    pair = token_bytes[idx-1] + token_bytes[idx]
+                    
+                    if pair not in pairs:
+                        pairs[pair] = 0
+
+                    pairs[pair] += count
 
 
+        print(f"Pairs : {pairs}")
+
+        return bytes_count_merged
+
+    def train(self, corpus : List[str]) -> Dict:
+
+        vocab = {}
+
+        pre_tokens : List = self._pretokenize(corpus = corpus)
+        print(f"Pre-tokens : {pre_tokens} ")
+
+        counts : Dict[str, int] = self._count(pre_tokens)
+        print(f"Counts : {counts}")
+
+        bytes_count = self._counts_to_bytes(counts)
+        print(f"Bytes Count : {bytes_count}")
+
+        bytes_count_merged = self._merge(bytes_count)
+
+        return vocab
+
+
+if __name__ == "__main__":
+
+    bpe = BPE()
+
+    bpe.train(examples)
+
+    
     
