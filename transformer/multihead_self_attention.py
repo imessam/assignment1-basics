601823162f2ecff1f1f09bcc0cce4bd30ce081b2
Mohamed Essam <imessam1998@gmail.com>
2026-08-07 19:28:41 +0300

    test pass

--- transformer/multihead_self_attention.py
+++ transformer/multihead_self_attention.py
@@ -6,6 +6,32 @@ from transformer.rope import RoPE
 from transformer.scaled_dot_product import ScaledDotProduct
 from transformer.linear import Linear
 
+
+class MultiHead(nn.Module):
+
+    def __init__(
+            self,
+            ) -> None:
+        
+        super().__init__()
+
+        self._scaled_dot_product = ScaledDotProduct()
+
+    def forward(self, Q : torch.Tensor, K : torch.Tensor, V : torch.Tensor) -> torch.Tensor:
+
+        batch_size, num_heads, seq_len, d_k, = Q.shape
+        _, _, _, d_v = V.shape
+
+        attn_scores_multi = torch.zeros(size = (batch_size, num_heads, seq_len, d_v))
+
+        masks = torch.triu(torch.ones(size = (seq_len, seq_len)), diagonal = 1)
+        masks = torch.where(masks == 0, True, False)
+        print(f"masks : {masks.shape}")
+
+        attn_scores_multi = self._scaled_dot_product(Q, K, V, masks)
+
+        return attn_scores_multi
+
 class MultiHeadSelfAttention(nn.Module):
 
     def __init__(
@@ -31,7 +57,9 @@ class MultiHeadSelfAttention(nn.Module):
         self._wo = Linear(in_features = (self._num_heads * self._d_v), out_features = self._d_model)
 
         self._rope = RoPE(theta = self._theta, d_k = self._d_k, max_seq_len = self._max_seq_len)
-        self._scaled_dot_product = ScaledDotProduct()
+        # self._scaled_dot_product = ScaledDotProduct()
+
+        self._multihead = MultiHead()
 
     def forward(self, x : torch.Tensor) -> torch.Tensor:
 
@@ -40,25 +68,32 @@ class MultiHeadSelfAttention(nn.Module):
         attn_scores = torch.Tensor()
 
         q : torch.Tensor = self._wq(x)
-        q_h = q.view(size = (batch_size, self._num_heads, seq_len, -1))
-        print(f"q : {q.shape} , q_h : {q_h.shape}")
+        q = q.view(size = (batch_size, seq_len, self._num_heads, -1))
+        q = q.transpose(1, 2)
+        print(f"q : {q.shape}")
 
-        k = self._wk(x)
-        k_h = k.view(size = (batch_size, self._num_heads, seq_len, -1))
-        print(f"k : {k.shape} , k_h : {k_h.shape}")
+        k : torch.Tensor = self._wk(x)
+        k = k.view(size = (batch_size, seq_len, self._num_heads, -1))
+        k = k.transpose(1, 2)
+        print(f"k : {k.shape}")
 
-        v = self._wv(x)
-        v_h = v.view(size = (batch_size, self._num_heads, seq_len, -1))
-        print(f"v : {v.shape} , v_h : {v_h.shape}")
+        v : torch.Tensor = self._wv(x)
+        v = v.view(size = (batch_size, seq_len, self._num_heads, -1))
+        v = v.transpose(1, 2)
+        print(f"v : {v.shape} ")
 
-        masks = torch.triu(torch.ones(size = (seq_len, seq_len)), diagonal = 1)
-        masks = torch.where(masks == 0, True, False)
-        print(f"masks : {masks.shape}")
+        # masks = torch.triu(torch.ones(size = (seq_len, seq_len)), diagonal = 1)
+        # masks = torch.where(masks == 0, True, False)
+        # print(f"masks : {masks.shape}")
 
-        attn_scores_h : torch.Tensor = self._scaled_dot_product(q_h, k_h, v_h, masks)
-        print(f"attn_scores_h : {attn_scores_h.shape}")
+        # attn_scores_h : torch.Tensor = self._scaled_dot_product(q_h, k_h, v_h, masks)
+        # print(f"attn_scores_h : {attn_scores_h.shape}")
 
-        attn_scores = attn_scores_h.view(batch_size, seq_len, -1)
+        attn_scores_multi : torch.Tensor = self._multihead(q, k, v)
+        attn_scores_multi = attn_scores_multi.transpose(1, 2)
+        print(f"attn_scores_multi : {attn_scores_multi.shape}")
+
+        attn_scores = attn_scores_multi.contiguous().view(batch_size, seq_len, -1)
         print(f"attn_scores : {attn_scores.shape}")
 
         return self._wo(attn_scores)
